c37ad837c12f36c57964ef3a6faf3b10f0e487d8
casali1 <frank_casali@hotmail.com>
2018-08-18 16:48:28 -0600

    Add project files.

---
+++ StringLibraryTest/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using StringLibrary;
+
+namespace StringLibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestStartsWithUpper()
+        {
+            // Tests that we expect to return true.
+            string[] words = { "Alphabet", "Zebra", "ABC", "Αθήνα", "Москва" };
+            foreach (var word in words)
+            {
+                bool result = word.StartsWithUpper();
+                Assert.IsTrue(result, String.Format("Expected for '{0}': true; Actual: {1}", word, result));
+            }
+        }
+
+        [TestMethod]
+        [DataRow("alphabet")]
+        [DataRow("error")]
+        [DataRow("αυτοκινητοβιομηχανία")]
+        [DataRow(".")]
+        public void TestDoesNotStartWithUpper(string someString)
+        {
+            // Tests that we expect to return false.
+                bool result = someString.StartsWithUpper();
+                Assert.IsFalse(result, String.Format("Expected for '{0}': false; Actual: {1}", someString, result));
+        }
+
+        [TestMethod]
+        [DataRow("string.Empty")]
+        [DataRow(null)]
+        public void DirectCallWithNullOrEmpty(string someString)
+        {
+            // Tests that we expect to return false.
+            bool result = StringLibrary.StringLibrary.StartsWithUpper(someString);
+            Assert.IsFalse(result, String.Format("Expected for '{0}': false; Actual: {1}", someString == null ? "<null>" : someString, result));
+        }
+    }
+}
